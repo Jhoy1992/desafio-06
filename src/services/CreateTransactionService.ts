cd8b475e7430b4b342c7e7a24b874c374c90b7cc
Jhoy1992 <jhonatanascari@gmail.com>
2020-06-10 15:07:05 -0300

    feat: challenge 6

--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,10 +1,53 @@
-// import AppError from '../errors/AppError';
+import { getCustomRepository } from 'typeorm';
+import AppError from '../errors/AppError';
 
 import Transaction from '../models/Transaction';
+import TransactionsRepository from '../repositories/TransactionsRepository';
+import CategoriesRepository from '../repositories/CategoriesRepository';
+
+interface Request {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+  category: string;
+}
 
 class CreateTransactionService {
-  public async execute(): Promise<Transaction> {
-    // TODO
+  public async execute({
+    title,
+    value,
+    type,
+    category,
+  }: Request): Promise<Transaction> {
+    const transactionsRepository = getCustomRepository(TransactionsRepository);
+    const categoriesRepository = getCustomRepository(CategoriesRepository);
+
+    const findCategory = await categoriesRepository.findOrCreate(category);
+
+    const checkTransactionType = ['income', 'outcome'].includes(type);
+
+    if (!checkTransactionType) {
+      throw new AppError('Transaction type is invalid');
+    }
+
+    if (type === 'outcome') {
+      const { total } = await transactionsRepository.getBalance();
+
+      if (total < value) {
+        throw new AppError('Outcome value cannot be more than total available');
+      }
+    }
+
+    const transaction = transactionsRepository.create({
+      title,
+      value,
+      type,
+      category_id: findCategory.id,
+    });
+
+    await transactionsRepository.save(transaction);
+
+    return transaction;
   }
 }
 
--- src/services/ImportTransactionsService.ts
+++ src/services/ImportTransactionsService.ts
@@ -1,8 +1,78 @@
+import { getRepository, getCustomRepository } from 'typeorm';
+import csvParse from 'csv-parse';
+import fs from 'fs';
+import path from 'path';
+import uploadConfig from '../config/upload';
+import AppError from '../errors/AppError';
+
 import Transaction from '../models/Transaction';
+import CategoriesRepository from '../repositories/CategoriesRepository';
+
+interface Request {
+  csvFilename: string;
+}
+
+interface NewTransaction {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+  category: string;
+}
 
 class ImportTransactionsService {
-  async execute(): Promise<Transaction[]> {
-    // TODO
+  private async loadCSV(filePath: string): Promise<NewTransaction[]> {
+    const readCSVStream = fs.createReadStream(filePath);
+
+    const parseStream = csvParse({ from_line: 2, ltrim: true, rtrim: true });
+
+    const parseCSV = readCSVStream.pipe(parseStream);
+
+    const lines: NewTransaction[] = [];
+
+    parseCSV.on('data', line => {
+      lines.push({
+        title: line[0],
+        value: Number(line[2]),
+        type: line[1],
+        category: line[3],
+      });
+    });
+
+    await new Promise(resolve => {
+      parseCSV.on('end', resolve);
+    });
+
+    return lines;
+  }
+
+  async execute({ csvFilename }: Request): Promise<Transaction[]> {
+    const transactionsRepository = getRepository(Transaction);
+    const categoriesRepository = getCustomRepository(CategoriesRepository);
+
+    const csvFilePath = path.join(uploadConfig.directory, csvFilename);
+
+    const transactionsArray = await this.loadCSV(csvFilePath);
+
+    const promisesTransactions = Promise.all(
+      transactionsArray.map(async transaction => {
+        const { title, type, value, category } = transaction;
+
+        const findCategory = await categoriesRepository.findOrCreate(category);
+
+        const newTransaction = transactionsRepository.create({
+          title,
+          type,
+          value,
+          category_id: findCategory.id,
+        });
+
+        await transactionsRepository.save(newTransaction);
+
+        return newTransaction;
+      }),
+    );
+
+    return promisesTransactions;
   }
 }
 
